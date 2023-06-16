74fb00e02259a37182cf875cf9be3e5ddd65dd96
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-15 18:45:47 -0700

    ff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* this is my c program */
+/* 
+ * this is my c program 
+ * */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
