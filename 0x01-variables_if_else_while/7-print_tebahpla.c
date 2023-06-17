d11828ea97b8ea1cab81993278d35225ec414c1a
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-16 19:01:55 -0700

    all done i hope

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all digits from 0 to 9
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+
+int i;
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
