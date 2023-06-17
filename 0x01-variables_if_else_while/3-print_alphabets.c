a0d51bc6f6df3ffc7645ac5f30a7b6268f57ca4d
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-16 17:27:19 -0700

    ff

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints alphbet in upper and lower case
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char str[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (i = 0; i < strlen(str); i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+return (0);
+}
