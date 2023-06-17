25f3de39cb46dcf187ba6f49e677428533d98545
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-16 18:26:02 -0700

    task 6 again

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 /**
  * main - print all digits from 0 to 9
  * Return: 0 (success)
@@ -7,11 +6,10 @@
 
 int main(void)
 {
-char nums[] = "0123456789";
-unsigned int i;
-for (i = 0; i < strlen(nums); i++)
+int i;
+for (i = 48; i < 58; i++)
 {
-putchar(nums[i]);
+putchar(i);
 }
 putchar('\n');
 return (0);
