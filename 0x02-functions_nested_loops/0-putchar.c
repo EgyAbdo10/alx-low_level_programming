b1fd12f48cfaa4ef840a1a957beace6afe757842
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-20 15:08:59 -0700

    dd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - print _putchar
+ * Return: (0) success
+ */
+int main(void)
+{
+unsigned int i;
+char text[] = "_putchar";
+for(i = 0; i < strlen(text); i++)
+{
+_putchar(text[i]);
+}
+_putchar("\n");
+return (0);
+}
