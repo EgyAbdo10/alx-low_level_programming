891170c17dfa7ac02381a3f8a349e86cb0080727
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-20 15:31:41 -0700

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * main - print the alpahbet
+ * Return: (0) success
+ */
+int main(void)
+{
+char i; 
+for (i = "a"; i <= "z"; i++)
+{
+_putchar(i);
+}
+_putchar('\n')
+return (0);
+}
