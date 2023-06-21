5925681594e19993f663f3b88a0f0f309dfbc99d
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-20 18:03:07 -0700

    ff

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - print the sign of the passed number
+ * Return: - 1 or 0 or -1
+ * @n: the number passed
+ */
+int print_last_digit(int n)
+{
+n = n % 10;
+return (n); 
+}
