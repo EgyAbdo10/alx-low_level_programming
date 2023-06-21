fbf48e393921bb5efbeddeb0c343491972a4aabf
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-20 19:35:19 -0700

    f

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - print the sign of the passed number
+ * Return: - void
+ * @void: the number passed
+ */
+void times_table(void)
+{
+int i;
+int b;
+int res;
+for (i = 0; i <= 9; i++)
+{
+for (b = 0; b <= 9; b++)
+{
+res = i * b;
+_putchar(res);
+if (b != 9)
+_putchar(44);
+}
+_putchar('\n')
+}
+}
