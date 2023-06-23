ffcfc81ea6e30ad41d14fbb6d897fd796f826cc3
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-22 19:32:31 -0700

    f

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * fizzbuzz - check the letter case
+ * @void: second number
+ * Return: 1 if upeer and 0 if lower
+ */
+
+void fizzbuzz(void)
+{
+int i;
+printf("%d ", 0);
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
+putchar('\n');
+}
+
+void main(void)
+{
+fizzbuzz();
+}
