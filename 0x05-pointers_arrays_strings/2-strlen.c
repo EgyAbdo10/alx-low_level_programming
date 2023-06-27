f8f20cb6f5a1080d33d3e374784e069f8f89141d
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-26 17:37:44 -0700

    fff

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - retest the number to 98
+ * @s: the first number you want to swap
+ * Return: void
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+return (length);
+}
