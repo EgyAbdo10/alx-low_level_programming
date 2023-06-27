33e08903e357522b2e1368ecde6fe2f3ba6647e3
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-26 19:34:38 -0700

    dd

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,8 @@
-#include "main.h"
-/**
- * print_rev - retest the number to 98
- * @s: the first number you want to swap
- * Return: void
- */
 void print_rev(char *s)
 {
 int length = 0;
 while (s[length] != '\0')
 {
-//putchar(s[length]);
 length++;
 }
 while (length >= 0)
