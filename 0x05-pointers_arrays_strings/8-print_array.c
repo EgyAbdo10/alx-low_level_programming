46fa2c506fbae62b82cf35d36de86e1aeb11b033
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-27 22:27:05 -0700

    ff

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - reverse string
+ * @a: the integer array
+ * @n: the number of elements
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+for (i = 0; i < n; i++)
+{
+if (i == (n - 1))
+{
+printf("%d", a[i]);
+}
+else if (i != (n - 1))
+{
+printf("%d, ", a[i]);
+}
+}
+printf('\n');
+}
