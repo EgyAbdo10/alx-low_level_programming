0c7b2ecb01841eb8350430b9e1ab7a5e49f2dde2
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-27 19:35:13 -0700

    dd

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: the string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int length = 0;
+int j = 0;
+char temp;
+while (s[length] != '\0')
+{
+length++;
+}
+length -= 1;
+while (length > (j / 2 + 1))
+{
+temp = s[length];
+s[length] = s[j];
+s[j] = temp;
+length--;
+j++;
+}
+}
