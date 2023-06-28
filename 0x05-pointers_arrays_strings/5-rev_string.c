7dd3f51d2117ac969b01a63af0098ed072e9b7a9
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-27 19:45:21 -0700

    ss

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,14 @@ void rev_string(char *s)
 int length = 0;
 int j = 0;
 char temp;
+int length2;
 while (s[length] != '\0')
 {
 length++;
 }
 length -= 1;
-while (length > (j / 2 + 2))
+length2 = length;
+while (length > (length2 / 2))
 {
 temp = s[length];
 s[length] = s[j];
