af38a15e2dc2d2e47f3842c7ce74412453abbd86
EgyAbdo10 <egyabdo490@gmail.com>
2023-06-30 22:47:50 -0700

    dd

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string - cancatenate
+ * @a: first str
+ * Return: the concatenated pointer str
+ */
+char *cap_string(char *a)
+{
+int i = 0;
+int j;
+int sep[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+while (a[i] != '\0')
+{
+if (a[i] <= 122 && a[i] >= 97)
+{
+for (j = 0; j < 13; j++)
+{
+if (a[i - 1] == sep[j] || i == 0)
+{
+a[i] -= 32;
+}
+}
+}
+i++;
+}
+return (a);
+}
