c6d9f7e9e678f138bdf48f0faa08e69695248637
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-03 16:43:08 -0700

    dd

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - blabla
+ * @s: first
+ * @c: second
+ * Return: dest after modification
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i])
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+i++;
+}
+return (NULL);
+}
