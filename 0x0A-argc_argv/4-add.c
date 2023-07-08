d40be4ee9ebcaceb46ad876beeff8bdda7a2f392
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-08 00:28:37 -0700

    dd

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * main - prints the file name
  * @argc: nuber of args
@@ -10,20 +11,21 @@ int main(int argc, char *argv[])
 {
 int count;
 int sum = 0;
+unsigned int i;
 if (argc > 1)
 {
 for (count = 1; count < argc; count++)
 {
-if (atoi(argv[count]) == 0)
+for (i = 0; i < strlen(argv[count]); i++)
+{
+if (argv[count][i] < 48 || argv[count][i] > 57)
 {
 printf("Error\n");
 return (1);
 }
-else
-{
+}
 if (atoi(argv[count]) > 0)
 sum += atoi(argv[count]);
-}
 }
 printf("%d\n", sum);
 return (0);
