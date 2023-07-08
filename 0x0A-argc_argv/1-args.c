00e7336c4334c631d8cbc16ffaf32cf1bb1ec28e
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-07 23:18:24 -0700

    dd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the file name
+ * @argc: nuber of args
+ * @argv: arguments array
+ * Return: 0 when success
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+}
