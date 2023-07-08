309c765375fac027639a4745b86cdf5606ff7211
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-07 23:13:07 -0700

    ss

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the file name
+ * @argc: nuber of args
+ * @argv: arguments array
+ * Return: 0 when success
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
