c9c51172a66ce110dbffa9a3737e917425b0f71c
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-10 08:47:33 -0700

    not important

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
 /**
  * create_array - create an array of the same char
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 char *str;
 unsigned int i = 0;
-str = malloc(size + 1);
+str = malloc(size);
 if (str == NULL || size == 0)
 {
 return (NULL);
