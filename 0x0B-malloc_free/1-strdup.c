de3cb25cdf60d8600763cdc54a80a9df663933af
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-10 09:02:08 -0700

    not important

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - create an array of the same char
+ * @str: the size of the array
+ * Return: the final string on success
+ */
+char *_strdup(char *str)
+{
+int len = 0;
+int i;
+if (str == NULL)
+return (NULL);
+while (str[len] != 0)
+{
+len++;
+}
+char *new_str;
+new_str = malloc(sizeof(char) * len);
+if (new_str == NULL)
+return (NULL);
+for (i = 0; i <= len; i++)
+{
+new_str[i] = str[i];
+}
+return (new_str);
+}
