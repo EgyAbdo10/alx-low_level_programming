9b194166a6b9b6ad4be019850b799746bf71c02e
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-10 08:40:41 -0700

    dd

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - create an array of the same char
+ * @size: the size of the array
+ * @c: the char repeated
+ * Return: the final string on success
+ */
+char *create_array(unsigned int size, char c)
+{
+char *str;
+int i = 0;
+str = malloc(size + 1);
+if (str == NULL || size == 0)
+{
+return (NULL);
+}
+while (i < size)
+{
+str[i] = c;
+i++;
+}
+str[i] = 0;
+return (str);
+}
