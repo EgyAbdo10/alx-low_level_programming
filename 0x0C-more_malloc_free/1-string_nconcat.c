e8efb77141063d5b6f332c131a1feb19531c1c2a
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-13 04:22:32 -0700

    not important

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,21 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * malloc_checked - check the functionality of malloc
- * @b: the size ot the memory allocated
- * Return: A pointer to the allocated memory on success and exit on failure
+ * check_null - create an array of the same char
+ * @new_str: the size of the array
+ * Return: the final string on success
  */
-void *malloc_checked(unsigned int b)
+char *check_null(char *new_str)
 {
-int *ptr;
-ptr = malloc(b);
-if (ptr == NULL)
-{
-return (NULL);
+if (new_str == NULL)
+new_str = "";
+return (new_str);
 }
-return (ptr);
-}
-
 /**
  * string_nconcat - create an array of the same char
  * @s1: the size of the array
@@ -24,20 +19,21 @@ return (ptr);
  * @n: number of bytes taken from s2
  * Return: the final string on success
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i, f, j, all, start;
 char *new_str;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+s1 = check_null(s1);
+s2 = check_null(s2);
 i = strlen(s2);
 f = strlen(s1);
 all = i + f + 1;
 if (n >= (i))
 {
-new_str = malloc_checked(sizeof(char) * (all));
+new_str = malloc(sizeof(char) * (all));
+if (new_str == NULL)
+return (NULL);
 for (j = 0; j < f; j++)
 {
 new_str[j] = s1[j];
@@ -49,7 +45,9 @@ new_str[j] = s2[i];
 }
 else if (n < i)
 {
-new_str = malloc_checked(sizeof(char) * (n + i + 1));
+new_str = malloc(sizeof(char) * (n + i + 1));
+if (new_str == NULL)
+return (NULL);
 for (j = 0; j < f; j++)
 {
 new_str[j] = s1[j];
