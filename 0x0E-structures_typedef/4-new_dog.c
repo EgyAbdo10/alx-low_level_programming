1c24efa5ebe9b8567b5145a2677078259bc84d73
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-18 14:06:07 -0700

    not important

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *my_dog = malloc(sizeof(dog_t));
 if (my_dog == NULL)
+{
+free(my_dog);
 return NULL;
+}
 my_dog->name = name;
 my_dog->age = age;
 my_dog->owner = owner;
