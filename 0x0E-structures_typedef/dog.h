6621c11ac309c44cb68441ce80328973f25fc96f
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-18 11:23:21 -0700

    not important

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,12 @@ char *name;
 float age;
 char *owner;
 };
+/**
+ * init_dog - initialize a dog abject
+ * @d: the dog object
+ * @name: the name of teh dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
