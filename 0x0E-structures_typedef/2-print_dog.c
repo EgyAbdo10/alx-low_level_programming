94f2a21e2599cf54ceb80531eac519eb3a2f4a6b
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-18 13:17:51 -0700

    not important

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - initialize a dog abject
+ * @d: the dog object
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+exit(98);
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\n", d->name);
+if (d->age == 0)
+printf("Age: (nil)\n");
+else
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
