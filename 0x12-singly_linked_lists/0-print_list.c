7309bcc8c89a440171aeac704fa458a485b298a7
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-27 17:47:44 -0700

    not important

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+/**
+ * print_list - print all elements of a list
+ * @h: the head
+ * Return: the count of elements
+*/
+size_t print_list(const list_t *h)
+{
+int count = 0;
+const list_t *ptr;
+if (h == NULL)
+return NULL;
+ptr = h;
+while (ptr != NULL)
+{
+count++;
+if (ptr->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+printf("[%u] %s\n", ptr->len, ptr->str);
+ptr = ptr->next;
+}
+return (count);
+}
