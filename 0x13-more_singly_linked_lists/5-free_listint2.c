94c85052f5ff6294f92b4e8c9c2f736061cbb8ac
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-31 21:04:22 -0700

    not important

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - print all elements
+ * @head: a pointer to the head pointer
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *ptr;
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+ptr = (*head)->next;
+free(*head);
+*head = ptr;
+}
+*head = NULL;
+}
