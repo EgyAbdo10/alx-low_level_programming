e460caaf81e609d041deedb3b6baf35595cbf39f
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-31 18:07:41 -0700

    not important

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint - print all elements
+ * @h: the head pointer
+ * Return: unsigned int with the number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t count = 0;
+const listint_t *ptr = h;
+while (ptr != NULL)
+{
+count++;
+printf("%d\n", ptr->n);
+ptr = ptr->next;
+}
+return (count);
+}
