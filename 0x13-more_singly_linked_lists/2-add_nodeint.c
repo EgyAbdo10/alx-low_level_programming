425cd890d2c1e2b6efb71f62445e64ae11276724
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-31 19:33:25 -0700

    not important

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * add_nodeint - print all elements
+ * @head: a pointer to the head pointer
+ * @n: the new added number
+ * Return: a new pointer head
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *ptr = *head;
+if (head == NULL)
+return NULL;
+ptr->next = *head;
+ptr->n = n;
+*head = ptr;
+return (*head);
+}
