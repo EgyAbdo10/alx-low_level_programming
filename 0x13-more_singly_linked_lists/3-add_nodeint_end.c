cc696e8a84f80cb83292085912422c07203a22e8
EgyAbdo10 <egyabdo490@gmail.com>
2023-07-31 20:55:14 -0700

    not important

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,15 +10,18 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 listint_t *ptr = malloc(sizeof(listint_t));
 listint_t *new_head = *head;
 if (ptr == NULL)
-return NULL;
-if (*head == NULL)
-return (*head);
+return (NULL);
 ptr->n = n;
 ptr->next = NULL;
+if (*head == NULL)
+{
+*head = ptr;
+return (*head);
+}
 while (new_head->next != NULL)
 {
-new_head =new_head->next;
+new_head = new_head->next;
 }
 new_head->next = ptr;
-return (new_head);
+return (ptr);
 }
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - print all elements
+ * @head: the head pointer
+ */
+void free_listint(listint_t *head)
+{
+listint_t *ptr;
+while (ptr != NULL)
+{
+ptr = head->next;
+free(head);
+head = ptr;
+}
+}
