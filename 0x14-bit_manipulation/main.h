447a1c21a0818da5512e7bc4ef1868f082f316f2
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-03 15:28:19 -0700

    not important

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,16 +6,6 @@
 unsigned int binary_to_uint(const char *b);
 int _log(int base, int num);
 int power(int base, int exp);
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif
-
