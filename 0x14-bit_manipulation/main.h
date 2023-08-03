e964e4fa55307bcb5cfd224c05a7fcda9e6f54de
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-03 15:52:13 -0700

    not important

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 unsigned int binary_to_uint(const char *b);
 int _log(int base, int num);
-int power(int base, int exp);
+int power(int num, int exp);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 #endif
