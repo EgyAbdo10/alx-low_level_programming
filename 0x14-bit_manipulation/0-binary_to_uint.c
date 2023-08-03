c1df8a7c3396ee635a4bb0e1f46a0fb36f2285e7
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-03 12:25:05 -0700

    not important

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * power - raise a number to a specific power
+ * @base: the number
+ * @exp: the exponent
+ * Return: the number raised to a specific power
+ */
+int power(int base, int exp)
+{
+if (exp == 0)
+return (1);
+return (power(base, exp - 1) * base);
+}
+/**
+ * binary_to_uint - change binary to a normal number
+ * @b: the binary string
+ * Return: the number in the normal format
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int j, i = 0;
+unsigned int res = 0;
+if (b == NULL)
+return (0);
+while (b[i] != '\0')
+i++;
+i--;
+for (j = 0; i >= 0; i--, j++)
+{
+if (b[i] == '1')
+res += power(2, j);
+else if (b[i] == '0')
+continue;
+else
+return (0);
+}
+return (res);
+}
