d6901e4acec64613bd04ccfff35e3a0376b068f7
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-03 16:07:07 -0700

    not important

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,54 +1,28 @@
 #include "main.h"
-/**
- * power - raise a number to a specific power
- * @num: the number
- * @exp: the exponent
- * Return: the number raised to a specific power
- */
-int power(int num, int exp)
-{
-if (exp == 0)
-return (1);
-return (power(num, exp - 1) * num);
-}
-/**
- * _log - raise a number to a specific power
- * @base: the base
- * @num: the number under the log
- * Return: the log for a number if found or -1 if not
- */
-int _log(int base, int num)
-{
-int i = 0;
-for (; power(base, i) != num && i <= num; i++)
-continue;
-if (i > num)
-return (-1);
-else
-return (i);
-}
+
 /**
  * print_binary - print a number in binary format
  * @n: the number in the normal format
  */
 void print_binary(unsigned long int n)
 {
-unsigned long int i = n;
-unsigned long int iter, residual = n;
+int checker = 0;
+unsigned long int temp;
+int iter;
 if (n == 0)
 _putchar('0' + 0);
-while (residual != 0)
+for (iter = 63; iter >= 0; iter--)
 {
-for (; _log(2, i) == -1; i--)
-continue;
+temp = n >> iter;
+if ((temp & 1) == 1)
+{
 _putchar('0' + 1);
-residual -= i;
-
-for (iter = i - 1; iter > residual; iter--)
+checker = 1;
+}
+else
 {
-if (_log(2, iter) != -1)
+if (checker == 1)
 _putchar('0' + 0);
 }
-i = residual;
 }
 }
