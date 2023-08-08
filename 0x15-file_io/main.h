fb92da79d8d1e34f3a4713da3dcf9caa0ca85917
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-07 18:31:38 -0700

    not important

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,17 +6,4 @@
 #include <limits.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-int i;
-i = write(1, &c, 1);
-return (i);
-}
 #endif
