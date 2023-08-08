8c507e060158fd43bf9d505b7ff7ca77b50f5c81
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-07 19:33:00 -0700

    not important

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,12 +8,13 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd, j, i = 0;
-if (filename == NULL)
-return (-1);
 if (text_content == NULL)
 return (-1);
+if (text_content != NULL)
+{
 while (text_content[i])
 i++;
+}
 fd  = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
