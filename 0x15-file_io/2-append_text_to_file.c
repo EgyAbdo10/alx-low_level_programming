1141e7963dfd33813310927caa01f1645a0ed1e7
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-08 06:42:17 -0700

    not important

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,37 +7,20 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, j, length;
+FILE *file_ptr = fopen(filename, "a");
 
-if (filename == NULL)
-return (-1);
-
-fd = open(filename, O_WRONLY | O_APPEND);
-if (fd == -1)
+if (file_ptr == NULL)
 return (-1);
 
 if (text_content != NULL)
 {
-for (length = 0; filename[length];)
-length++;
+fputs(text_content, file_ptr);
 }
 
-else
-{
-close(fd);
-return (1);
-}
+fclose(file_ptr);
 
 if (chmod(filename, 0664) != 0)
 return (-1);
 
-j = write(fd, text_content, length);
-if (j == -1 || j != length)
-{
-close(fd);
-return (-1);
-}
-
-close(fd);
 return (1);
 }
