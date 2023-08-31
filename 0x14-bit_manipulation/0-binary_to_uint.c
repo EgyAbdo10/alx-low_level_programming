7c2a389bd4142b3a8f7205ea3417c99c61e12b0a
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-31 04:11:21 -0700

    not important

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,61 @@
 #include "main.h"
 /**
- * get_exp - raise a number to a specific power
- * @num: the number
+ * get_exp - get the result of an exp operation
+ * @num: the base number
  * @exp: the exponent
- * Return: the number raised to a specific power
+ * Return: the result
  */
 int get_exp(int num, int exp)
 {
+int i = 0;
+int res = 1;
 if (exp == 0)
+{
 return (1);
-return (power(num, exp - 1) * num);
+}
+else
+{
+for (; i < exp; i++)
+{
+res *= num;
+}
+}
+return (res);
 }
 /**
- * binary_to_uint - change binary to a normal number
- * @b: the binary string
- * Return: the number in the normal format
+ * my_strlen - get the length of a string
+ * @str: the string
+ * Return: the length of the string
+ */
+int my_strlen(const char *str)
+{
+int i = 0;
+if (str == NULL)
+return (0);
+while (str[i] != '\0')
+i++;
+return (i);
+}
+/**
+ * binary_to_uint - change binary to base 10
+ * @b: the number string in the binary format
+ * Return: the equivalint base 10 number or 0 if failed
  */
 unsigned int binary_to_uint(const char *b)
 {
-int j, i = 0;
+int i = 0;
 unsigned int res = 0;
 if (b == NULL)
 return (0);
-while (b[i] != '\0')
-i++;
-i--;
-for (j = 0; i >= 0; i--, j++)
+int len = my_strlen(b);
+for (; b[i] != '\0'; i++, len--)
 {
-if (b[i] == '1')
-res += get_exp(2, j);
-else if (b[i] == '0')
-continue;
-else
+if (b[i] != '1' && b[i] != '0')
 return (0);
+if (b[i] == '1')
+{
+res += get_exp(2, (len - 1));
+}
 }
 return (res);
 }
