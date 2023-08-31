f4233b4503281b214217c013421147cb5611482e
EgyAbdo10 <egyabdo490@gmail.com>
2023-08-31 14:24:57 -0700

    not important

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * get_exp - get the result of an exp operation
+ * @num: the base number
+ * @exp: the exponent
+ * Return: the result
+ */
+int get_exp(int num, int exp)
+{
+int i = 0;
+int res = 1;
+if (exp == 0)
+{
+return (1);
+}
+else
+{
+for (; i < exp; i++)
+{
+res *= num;
+}
+}
+return (res);
+}
+/**
+ * set_bit - set a bit to 1 at a given index
+ * @n: a pointer to the number
+ * @inedx: the index of the bit
+ * Return: 1 on success or 0 on failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int temp_num;
+if (index > 63)
+return (0);
+temp_num = *n >> index;
+if ((temp_num & 1) == 0)
+*n += get_exp(2, index);
+return (1);
+}
