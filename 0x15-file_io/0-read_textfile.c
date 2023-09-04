51c2777c6dc7ad3178b3ff162c734efa60fdb362
EgyAbdo10 <egyabdo490@gmail.com>
2023-09-04 05:07:42 -0700

    not important

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,20 +7,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t con_len_read, con_len_write;
-    char *content = malloc(sizeof(char) * letters);
-    int fd;
-    if (filename == NULL)
-    return (0);
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    return (0);
-    con_len_read = read(fd, content, letters);
-    if (con_len_read == -1)
-    return (0);
-    con_len_write = write(STDOUT_FILENO, content, con_len_read);
-    if (con_len_write != con_len_read)
-    return (0);
-
-    return (con_len_write);
+ssize_t con_len_read, con_len_write;
+char *content = malloc(sizeof(char) * letters);
+int fd;
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+con_len_read = read(fd, content, letters);
+if (con_len_read == -1)
+return (0);
+con_len_write = write(STDOUT_FILENO, content, con_len_read);
+if (con_len_write != con_len_read)
+return (0);
+else
+return (con_len_write);
 }
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+this is the ALX file i/o project
