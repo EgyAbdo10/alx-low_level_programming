e27e6222201020e552c6798a262d4450a69a0bf8
EgyAbdo10 <egyabdo490@gmail.com>
2023-10-12 19:12:18 -0700

    not important

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum dtaa in nodes
+ * @head: the head pointer
+ * Return: the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+if (head == NULL)
+return (0);
+while (head != NULL)
+{
+sum++;
+head = head->next;
+}
+return (sum);
+}
