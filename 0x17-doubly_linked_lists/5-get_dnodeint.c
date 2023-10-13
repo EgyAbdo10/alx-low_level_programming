2a370a07362c4e269f8979c0ddcc22ae85057afe
EgyAbdo10 <egyabdo490@gmail.com>
2023-10-12 18:57:08 -0700

    not important

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+int get_dll_len(dlistint_t *head)
+{
+int i = 0;
+dlistint_t *ptr = head;
+int len;
+while ((ptr) != NULL)
+{
+i++;
+ptr = ptr->next;
+}
+return (i);
+}
+/**
+ * get_dnodeint_at_index - get node at index
+ * @head: the head pointer
+ * @index: the index of the node starting from zero
+ * Return a pointer to the node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+int i = 0;
+if (get_dll_len(head) <= index)
+return (NULL);
+while (i < index)
+{
+head = head->next;
+i++;
+}
+return (head);
+}
