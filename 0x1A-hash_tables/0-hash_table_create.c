502bb826f1f3fd9f477e011e29a919213ae43881
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-03 17:09:16 -0700

    not important

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,8 @@ hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 if (hash_table == NULL)
 return (NULL);
 hash_table->size = size;
-hash_table->array = malloc(size * sizeof(hash_node_t));
+hash_table->array = malloc(size * sizeof(hash_node_t *));
 if (hash_table->array == NULL)
 return (NULL);
-return hash_table;
+return (hash_table);
 }
