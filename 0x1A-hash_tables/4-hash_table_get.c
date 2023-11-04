f0e440e2bd03ae40c2b9eab9586837a228ac5e95
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-03 19:10:50 -0700

    not important

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - look up a value
+ * @ht: the hash table
+ * @key: the key to the node
+ * Return: the value associated to the key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+int index = key_index(hash_djb2(key), ht->array);
+hash_node_t *tmp = ht->array[index];
+while (tmp != NULL)
+{
+if (strcmp(tmp->key, key) == 0)
+{
+return (tmp->value);
+}
+tmp = tmp->next;
+}
+return NULL;    
+}
+
