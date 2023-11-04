78a9d8d4e81d72aa03ead34a2b0ce8fdcdf66021
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-04 06:33:42 -0700

    not important

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,14 +7,12 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-int index = key_index(hash_djb2(key), ht->array);
+unsigned long int index = key_index(hash_djb2(key), ht->array);
 hash_node_t *tmp = ht->array[index];
 while (tmp != NULL)
 {
 if (strcmp(tmp->key, key) == 0)
-{
-return (tmp->value);
-}
+return ((char *)tmp->value);
 tmp = tmp->next;
 }
 return NULL;    
