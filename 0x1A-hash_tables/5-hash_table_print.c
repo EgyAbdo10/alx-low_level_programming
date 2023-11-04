d0d75aed32166d88767467d8918f95c889258c21
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-04 10:16:59 -0700

    not important

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    int printed_first = 0;
+    hash_node_t *tmp;
+    if (ht == NULL)
+    return;
+    printf("{");
+    for (i = 0; i < (ht->size); i++)
+    {
+        tmp = ht->array[i];
+        
+        while (tmp != NULL)
+        {
+            printf("'%s': '%s'", tmp->key, tmp->value);
+            printed_first = 1;
+            tmp = tmp->next;
+        }
+        if (i != (ht->size) - 1 && printed_first == 1)
+        {
+        if (ht->array[i + 1] != NULL)
+        printf(", ");
+        }
+    }
+    printf("}\n");
+}
