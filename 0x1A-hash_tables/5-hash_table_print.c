a0860aeda02fb8b79a903c5a2e0a2b3e6653a2c8
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-04 10:22:09 -0700

    not important

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,29 +1,34 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_print - print hash tabe keys and values
+ * @ht: a pointer to the hash table
+ */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    int printed_first = 0;
-    hash_node_t *tmp;
-    if (ht == NULL)
-    return;
-    printf("{");
-    for (i = 0; i < (ht->size); i++)
-    {
-        tmp = ht->array[i];
-        
-        while (tmp != NULL)
-        {
-            printf("'%s': '%s'", tmp->key, tmp->value);
-            printed_first = 1;
-            tmp = tmp->next;
-        }
-        if (i != (ht->size) - 1 && printed_first == 1)
-        {
-        if (ht->array[i + 1] != NULL)
-        printf(", ");
-        }
-    }
-    printf("}\n");
+unsigned long int i;
+int printed_first = 0;
+hash_node_t *tmp;
+if (ht == NULL)
+return;
+printf("{");
+for (i = 0; i < (ht->size); i++)
+{
+tmp = ht->array[i];
+
+while (tmp != NULL)
+{
+printf("'%s': '%s'", tmp->key, tmp->value);
+printed_first = 1;
+if (tmp->next != NULL)
+printf(", ");
+tmp = tmp->next;
+}
+if (i != (ht->size) - 1 && printed_first == 1)
+{
+if (ht->array[i + 1] != NULL)
+printf(", ");
+}
+}
+printf("}\n");
 }
