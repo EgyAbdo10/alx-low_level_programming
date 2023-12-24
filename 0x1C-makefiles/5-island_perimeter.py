bd920dd0df02eabffeda5c672e9431aa5fc053af
EgyAbdo10 <egyabdo490@gmail.com>
2023-12-25 00:47:32 +0200

    added or modified a file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,18 +6,15 @@ for calculating the perimter of an island"""
 
 def island_perimeter(grid):
     """calculate the perimeter of an island"""
-    n_iteration = len(grid)
     connections = 0
     total_boxes = 0
-    if len(grid[0]) < len(grid):
-        n_iteration = len(grid[0])
     for i in range(len(grid)):
         total_boxes += grid[i].count(1)
-        for row_index in range(len(grid[i]) - 1):
-            if (grid[i][row_index] == 1 and grid[i][row_index + 1] == 1):
+        for idx_in_row in range(len(grid[i]) - 1):
+            if (grid[i][idx_in_row] == 1 and grid[i][idx_in_row + 1] == 1):
                 connections += 1
     for i in range(len(grid[0])):
-        for col_idx in range(len(grid) - 1):
-            if (grid[col_idx][i] == 1 and grid[col_idx + 1][i] == 1):
+        for idx_in_col in range(len(grid) - 1):
+            if (grid[idx_in_col][i] == 1 and grid[idx_in_col + 1][i] == 1):
                 connections += 1
     return (total_boxes * 4) - (connections * 2)
