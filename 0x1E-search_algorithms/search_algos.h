8cbbfa580cd9e91b043aa13d189228002eca096f
EgyAbdo10 <egyabdo490@gmail.com>
2024-07-25 01:10:44 +0300

    not important

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,7 @@ int binary_search_recursive(int *array, size_t size, int value,
 int start_idx, int end_idx);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int interpolation_search_recursive(int *array, size_t size,
+int value, int low_idx, int high_idx);
 
 #endif
